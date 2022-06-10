1edd7952673e5dccf861f27d6ef535f8958e52f5
Maksim.Panasenko <maks.panasenko01@bk.ru>
2022-06-10 19:43:17 +0300

    refactoring

--- MovieAPIs/UnofficialKinopoiskApiClient.cs
+++ MovieAPIs/UnofficialKinopoiskApiClient.cs
@@ -1,30 +1,34 @@
-﻿using System.Net;
-using System.Net.Http.Headers;
+﻿using System.Net.Http.Headers;
 using System.Text.Json;
 
 namespace MovieAPIs
 {
-    public class UnofficialKinopoiskApi
+    public class UnofficialKinopoiskApiClient
     {
-        const string baseUrl = "https://kinopoiskapiunofficial.tech/api/v2.2";
+        const string baseApiUrl = "https://kinopoiskapiunofficial.tech/api";
         const string films = "films";
+        const string lastApiVersion = "v2.2";
         readonly HttpClient client;
         readonly JsonSerializerOptions jsonSerializerOptions;
+        readonly string apiVersion;
 
-        public UnofficialKinopoiskApi(string token)
+        public UnofficialKinopoiskApiClient(string apiKey, string apiVersion)
         {
             client = new HttpClient();
             client.DefaultRequestHeaders.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Add("X-API-KEY", token);
+            client.DefaultRequestHeaders.Add("X-API-KEY", apiKey);
             jsonSerializerOptions = new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true
             };
+            this.apiVersion = apiVersion;
         }
+        public UnofficialKinopoiskApiClient(string apiKey)
+            : this(apiKey, lastApiVersion) { }
         public async Task<Film> GetFilmByIdAsync(int id)
         {
-            string requestUrl = $"{baseUrl}/{films}/{id}";
+            string requestUrl = $"{baseApiUrl}/{apiVersion}/{films}/{id}";
             var responceBody = await client.GetStreamAsync(requestUrl);
             var film = JsonSerializer.Deserialize<Film>(responceBody, jsonSerializerOptions);
             return film;
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -8,8 +8,6 @@ namespace TestConsole
     {
         static async Task Main(string[] args)
         {
-            var client = new UnofficialKinopoiskApi("cfbe1246-e6eb-4ae8-bbd0-6fb17b9223b8");
-            Film film = await client.GetFilmByIdAsync(301);
         }
     }
 }
