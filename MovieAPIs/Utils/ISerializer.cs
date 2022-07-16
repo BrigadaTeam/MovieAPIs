159be09ff84b98efae6a8cfeee1fe378c05ab9b5
Maks <60705322+Maksim-Trolina@users.noreply.github.com>
2022-07-17 02:23:43 +0300

    Add tests (#16)
    * refactoring UrlHelperTests
    
    * added new tests
    
    * fix text for exception messages
    
    * for remove warnings
    
    * remove dotnet5
    
    * refactoring JsonConfiguration

--- MovieAPIs/Utils/ISerializer.cs
+++ MovieAPIs/Utils/ISerializer.cs
@@ -1,9 +1,23 @@
 ﻿using System.IO;
+using System.Text.Json;
 
 namespace MovieAPIs.Utils
 {
     internal interface ISerializer
     {
         internal T Deserialize<T>(string json);
+    }
+
+    internal class DotNetJsonSerializer : ISerializer
+    {
+        readonly JsonSerializerOptions options;
+        internal DotNetJsonSerializer(JsonSerializerOptions options)
+        {
+            this.options = options;
+        }
+        T ISerializer.Deserialize<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json, options)!;
+        }
     }
 }
