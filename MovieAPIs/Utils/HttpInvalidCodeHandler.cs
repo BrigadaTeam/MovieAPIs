b94c20add15d1e5fe3654a7bf5d588511db41a18
EvgenKoulik <ursor63@mail.ru>
2022-07-30 23:25:33 +0300

    Refactoring

--- MovieAPIs/Utils/HttpInvalidCodeHandler.cs
+++ MovieAPIs/Utils/HttpInvalidCodeHandler.cs
@@ -1,21 +1,23 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-
 namespace MovieAPIs.Utils
 {
     internal static class HttpInvalidCodeHandler
     {
+        internal static readonly Dictionary<int, string> Errors = new Dictionary<int, string>()
+        {
+            {401, "Empty or invalid token"},
+            {402, "Request limit exceeded (either daily or total)"},
+            {404, "Data not found"},
+            {429, "Too many requests. General limit - 20 requests per second"}
+        };
         internal static void ThrowException(HttpStatusCode code)
         {
-            throw (int)code switch
-            {
-                401 => new HttpRequestException($"Empty or invalid token - {code}"),
-                402 => new HttpRequestException($"Request limit exceeded (either daily or total) - {code}"),
-                404 => new HttpRequestException($"Data not found - {code}"),
-                429 => new HttpRequestException($"Too many requests. General limit - 20 requests per second - {code}"),
-                _ => new Exception($"An error occurred with code {code}")
-            };
+            var exceptionMessage =
+                Errors.ContainsKey((int)code) ? $"{Errors[(int)code]}" : code.ToString();
+            
+            throw new HttpRequestException(exceptionMessage);
         }
     }
 }
