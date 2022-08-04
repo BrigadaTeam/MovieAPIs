aa3a1d9d112765ed35b6dcda284aa377df6c2940
EvgenKoulik <ursor63@mail.ru>
2022-08-04 16:02:25 +0300

    Add get staff by film id

--- MovieAPIs/UnofficialKinopoiskApiClient.cs
+++ MovieAPIs/UnofficialKinopoiskApiClient.cs
@@ -225,5 +225,21 @@ namespace MovieAPIs
             var filmsResponse = serializer.Deserialize<FilmsResponse<Season>>(responseBody);
             return filmsResponse;
         }
+        
+        public async Task<StaffResponse[]> GetStaffByFilmIdAsync(int filmId)
+        {
+            var queryParams = new Dictionary<string, string>
+            {
+                ["filmId"] = filmId.ToString(),
+            };
+            var staffUrl = configuration["UnofficialKinopoisk:V1:StaffUrl"];
+            var urlPathWithQuery = UrlHelper.GetPathWithQuery(queryParams, staffUrl);
+            var response = await httpClient.GetAsync(urlPathWithQuery);
+            if (!response.IsSuccessStatusCode) 
+                HttpInvalidCodeHandler.ThrowException(response.StatusCode);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var filmsResponse = serializer.Deserialize<StaffResponse[]>(responseBody);
+            return filmsResponse;
+        }
     }
 }
