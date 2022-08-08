0b2fc5ae9038f9bf104f04032e7581f0e60efded
EvgenKoulik <ursor63@mail.ru>
2022-08-08 20:37:43 +0300

    Add method

---
+++ MovieAPIs/Models/Responses/PersonResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieAPIs.Models
+{
+    public class PersonResponse : Person
+    {
+        public int HasAwards { get; set; }
+        public string[] Facts { get; set; }
+        public Spouse[] Spouses { get; set; }
+        public FilmPersonInfo[] Films { get; set; }
+    }
+}
+
