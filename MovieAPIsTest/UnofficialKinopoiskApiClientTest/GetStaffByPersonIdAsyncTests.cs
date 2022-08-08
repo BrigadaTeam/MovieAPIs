a61e6ea72236bf6c3e765fa8e4381e508a462c84
EvgenKoulik <ursor63@mail.ru>
2022-08-08 21:12:07 +0300

    Refactoring

---
+++ MovieAPIsTest/UnofficialKinopoiskApiClientTest/GetStaffByPersonIdAsyncTests.cs
@@ -0,0 +1,77 @@
+﻿using MovieAPIs.Utils;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using MovieAPIs;
+using Moq;
+using System.Net;
+using System.Net.Http;
+
+
+namespace MovieAPIsTest
+{
+    public class GetStaffByPersonIdAsyncTests
+    {
+        [Test]
+
+        public async Task GetStaffByPersonIdAsync_ExistingId_CorrectResult()
+        {
+            var response = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.Accepted,
+                Content = new StringContent(@"{""personId"":37859,""nameRu"":""Леонардо ДиКаприо""}"),
+            };
+            var url = "https://kinopoiskapiunofficial.tech/api/v1/staff/37859";
+            var httpClient = Mock.Of<IHttpClient>(x => x.GetAsync(url) == Task.FromResult<HttpResponseMessage>(response));
+            var client = new UnofficialKinopoiskApiClient(httpClient);
+            var staffByPersonIdAsync = client.GetStaffByPersonIdAsync(37859).Result;
+            Assert.IsTrue(staffByPersonIdAsync.PersonId == 37859 && staffByPersonIdAsync.NameRu == "Леонардо ДиКаприо");
+        }
+
+        /*[Test]
+        
+        public async Task GetStaffByFilmIdAsync_LongId_Exception()
+        {
+            var response = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+            };
+            var url = "https://kinopoiskapiunofficial.tech/api/v1/staff?filmId=99999999";
+            var httpClient = Mock.Of<IHttpClient>(x => x.GetAsync(url) == Task.FromResult<HttpResponseMessage>(response));
+            var client = new UnofficialKinopoiskApiClient(httpClient);
+            var ex = Assert.ThrowsAsync<HttpRequestException>(() => client.GetStaffByFilmIdAsync(99999999));
+            Assert.True(ex! == HttpInvalidCodeHandler.Errors[HttpStatusCode.BadRequest]);
+        }
+ 
+        [Test]
+        
+        public async Task GetStaffByFilmIdAsync_EmptyOrWrongToken_Exception()
+        {
+            var response = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.Unauthorized,
+            };
+            var url = "https://kinopoiskapiunofficial.tech/api/v1/staff?filmId=665";
+            var httpClient = Mock.Of<IHttpClient>(x => x.GetAsync(url) == Task.FromResult<HttpResponseMessage>(response));
+            var client = new UnofficialKinopoiskApiClient(httpClient);
+            var ex = Assert.ThrowsAsync<HttpRequestException>(() => client.GetStaffByFilmIdAsync(665));
+            Assert.True(ex! == HttpInvalidCodeHandler.Errors[HttpStatusCode.Unauthorized]);
+        }
+   
+        [Test]
+        
+        public async Task GetStaffByFilmIdAsync_NegativeId_Exception()
+        {
+            var response = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.NotFound,
+            };
+            var url = "https://kinopoiskapiunofficial.tech/api/v1/staff?filmId=-1";
+            var httpClient = Mock.Of<IHttpClient>(x => x.GetAsync(url) == Task.FromResult<HttpResponseMessage>(response));
+            var client = new UnofficialKinopoiskApiClient(httpClient);
+            var ex = Assert.ThrowsAsync<HttpRequestException>(() => client.GetStaffByFilmIdAsync(-1));
+            Assert.True(ex! == HttpInvalidCodeHandler.Errors[HttpStatusCode.NotFound]);
+        }*/
+        
+    }
+}
+
