9ec7b6de74e220f70b982d30bd508a29938c80a8
EvgenKoulik <ursor63@mail.ru>
2022-08-26 23:23:36 +0300

    Update HttpResponseMessageExtensions.cs

--- MovieAPIs/Utils/HttpResponseMessageExtensions.cs
+++ MovieAPIs/Utils/HttpResponseMessageExtensions.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MovieAPIs.Utils
 {
     internal static class HttpResponseMessageExtensions
     {
-        internal static Task<string> ReadAsStringContentOrThrowExceptionAsync(this HttpResponseMessage response)
+        internal static Task<string> ReadAsStringContentOrThrowExceptionAsync(this HttpResponseMessage response, CancellationToken ct)
         {
             if (!response.IsSuccessStatusCode)
                 HttpInvalidCodeHandler.ThrowException(response.StatusCode);
-            return response.Content.ReadAsStringAsync();
+            return response.Content.ReadAsStringAsync(ct);
         }
     }
 }
