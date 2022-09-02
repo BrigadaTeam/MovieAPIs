feef56346056e2179c0eba5e02824fd3c9a46e66
EvgenKoulik <ursor63@mail.ru>
2022-09-02 16:54:03 +0300

    Attempt to fix a bug

--- MovieAPIsTest/ManyRequestsHelperTests.cs
+++ MovieAPIsTest/ManyRequestsHelperTests.cs
@@ -2,6 +2,7 @@
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using MovieAPIs.Utils;
 using System.Net.Http;
@@ -32,7 +33,67 @@ namespace MovieAPIsTest
             };
         }
         
+         [Test]
+        public async Task QuickResponses()
+        {
+            var time = await Time(async () =>
+            {
+                IHttpClient httpClient = Mock.Of<IHttpClient>(x => x.GetAsync(It.IsAny<string>(), CancellationToken.None) == GetHttpMessageAsync(TimeSpan.FromMilliseconds(100)));
+                var manyRequests = new ManyRequestsHelper(httpClient, serializer);
+                int expectedCount = 13;
+                int requestCountInSecond = 5;
+                var dataFromAllPages = manyRequests.GetDataAsync<Nomination>(queryParams, requestCountInSecond, "testUrl", 1, expectedCount, CancellationToken.None);
+                int count = 0;
+                await foreach(var dataFromPage in dataFromAllPages)
+                {
+                    Assert.AreEqual(dataFromPage.Name, "Сатурн");
+                    count++;
+                }
+                Assert.AreEqual(expectedCount, count);
+            });
+            Assert.That(time, Is.GreaterThan(TimeSpan.FromSeconds(3))); // expectedCount(13) / requestCountInSecond(5) = min operating time(3)
+            Assert.That(time, Is.LessThan(TimeSpan.FromSeconds(3.2)));
+        }
+
         [Test]
+        public async Task SlowResponses()
+        {
+            var time = await Time(async () =>
+            {
+                IHttpClient httpClient = Mock.Of<IHttpClient>(x => x.GetAsync(It.IsAny<string>(), CancellationToken.None) == GetHttpMessageAsync(TimeSpan.FromMilliseconds(1200)));
+                var manyRequests = new ManyRequestsHelper(httpClient, serializer);
+                int expectedCount = 13;
+                int requestCountInSecond = 5;
+                var dataFromAllPages = manyRequests.GetDataAsync<Nomination>(queryParams, requestCountInSecond, "testUrl", 1, expectedCount, CancellationToken.None);
+                int count = 0;
+                await foreach (var dataFromPage in dataFromAllPages)
+                {
+                    Assert.AreEqual(dataFromPage.Name, "Сатурн");
+                    count++;
+                }
+                Assert.AreEqual(expectedCount, count);
+            });
+            Assert.That(time, Is.GreaterThan(TimeSpan.FromSeconds(3))); // expectedCount(13) / requestCountInSecond(5) = min operating time(3)
+            Assert.That(time, Is.LessThan(TimeSpan.FromSeconds(3.4)));
+        }
+
+        private async Task<HttpResponseMessage> GetHttpMessageAsync(TimeSpan time)
+        {
+            await Task.Delay(time);
+            return await Task.FromResult(new HttpResponseMessage { 
+                StatusCode = HttpStatusCode.Accepted,
+                Content = new StringContent(@"{""items"":[{""name"":""Сатурн""}]}")
+            });
+        }
+        private async Task<TimeSpan> Time(Func<Task> toTime)
+        {
+            var timer = Stopwatch.StartNew();
+            await toTime();
+            timer.Stop();
+            return timer.Elapsed;
+        }
+        
+        /*[Test]
         public async Task QuickResponses()
         {
             var time = await MovieApiTestHelper.GetWorkTime(async () =>
@@ -41,7 +102,7 @@ namespace MovieAPIsTest
                 var manyRequests = new ManyRequestsHelper(httpClient, serializer);
                 int expectedCount = 13;
                 int requestCountInSecond = 5;
-                var dataFromAllPages = manyRequests.GetDataAsync<Nomination>(queryParams, requestCountInSecond, "testUrl", 1, expectedCount,  CancellationToken.None);
+                var dataFromAllPages = manyRequests.GetDataAsync<Nomination>(queryParams, requestCountInSecond, "testUrl", 1, expectedCount, It.IsAny<CancellationToken>());
                 int count = 0;
                 await foreach(var dataFromPage in dataFromAllPages)
                 {
@@ -63,7 +124,7 @@ namespace MovieAPIsTest
                 var manyRequests = new ManyRequestsHelper(httpClient, serializer);
                 int expectedCount = 13;
                 int requestCountInSecond = 5;
-                var dataFromAllPages = manyRequests.GetDataAsync<Nomination>(queryParams, requestCountInSecond, "testUrl", 1, expectedCount, CancellationToken.None);
+                var dataFromAllPages = manyRequests.GetDataAsync<Nomination>(queryParams, requestCountInSecond, "testUrl", 1, expectedCount, It.IsAny<CancellationToken>());
                 int count = 0;
                 await foreach (var dataFromPage in dataFromAllPages)
                 {
@@ -74,6 +135,6 @@ namespace MovieAPIsTest
             });
             Assert.That(time, Is.GreaterThan(TimeSpan.FromSeconds(3))); // expectedCount(13) / requestCountInSecond(5) = min operating time(3)
             Assert.That(time, Is.LessThan(TimeSpan.FromSeconds(3.4)));
-        }
+        }*/
     }
 }
